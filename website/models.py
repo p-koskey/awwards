2ddede0f39f2305a96a19e4e9fabe8a1992998a4
p-koskey <patiencekoskey@gmail.com>
2020-10-25 23:14:50 +0300

    update profile model by adding signals

--- website/models.py
+++ website/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -12,3 +15,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Account'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
