033ff0babebff2309e06f54f2cc2c9c61990f219
p-koskey <patiencekoskey@gmail.com>
2020-10-26 22:48:21 +0300

    create function to show posts

--- website/views.py
+++ website/views.py
@@ -1,13 +1,27 @@
 from django.http  import HttpResponse
 from django.shortcuts import render, redirect
+from django.http  import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate
-from .forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm, PostForm
+from .models import Post,Profile, User
 # Create your views here.
 
 def welcome(request):
-     return render(request, 'index.html')
+    posts = Post.objects.all()
+    
+    if request.method == 'POST':
+        uform = PostForm(request.POST, request.FILES)
+        if uform.is_valid():
+            post = uform.save(commit=False)
+            post.user = request.user
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        uform = PostForm()
+    return render(request, 'index.html',{'uform': uform,'posts':posts})
+    
 
 def register(request):
     if request.method == 'POST':
