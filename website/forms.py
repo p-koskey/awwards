a3ace3cdd475fc100a4d7641f9f88095c37d27ec
p-koskey <patiencekoskey@gmail.com>
2020-10-26 22:51:04 +0300

    create form to upload projects

--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
+from .models import Post
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2',) 
+        fields = ('username', 'email', 'password1', 'password2',)
+
+class PostForm(forms.ModelForm):
+    picture = CloudinaryField('image')
+
+    class Meta:
+        model = Post
+        fields = ('picture', 'projectname', 'link', 'projectinfo', 'languages',) 
