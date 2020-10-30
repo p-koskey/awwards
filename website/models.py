8dc1edd8691a2ec0966cfcc60e6d7f048edcca40
p-koskey <patiencekoskey@gmail.com>
2020-10-31 02:08:23 +0300

    create tests

--- website/models.py
+++ website/models.py
@@ -41,7 +41,18 @@ class Post(models.Model):
         
         posts = cls.objects.filter( projectname__icontains=search_term)
         return posts
+     
+    def delete_post(self):
+        self.delete()
 
+    def save_post(self):
+        self.save()
+
+    @classmethod
+    def posts(cls):
+        return cls.objects.all()
+
+    
 class Rate(models.Model):
     design = models.IntegerField(null=True,default=0)
     usability = models.IntegerField(null=True,default=0)
@@ -50,5 +61,17 @@ class Rate(models.Model):
     user = models.ForeignKey(User,null = True, on_delete=models.CASCADE)
     post = models.ForeignKey(Post,related_name='rate',null=True, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.post} Rate'
 
+    def save_rate(self):
+        self.save()
+
+    def delete_rate(self):
+        self.delete()
+
+    @classmethod
+    def get_rates(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
     
